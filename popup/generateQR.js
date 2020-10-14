a1f1c0a849b92dcd2cfeda912eef9a0694c27a6f
Hanae Rateau <hrateau@uwaterloo.ca>
2020-10-14 17:38:20 -0400

    [manifest.json] renamed extension [generateQR.js] refactored

--- popup/generateQR.js
+++ popup/generateQR.js
@@ -2,15 +2,18 @@ function reportExecuteScriptError(error) {
   console.error(`Failed to execute beastify content script: ${error.message}`);
 }
 
+function generateQRCode(){
+  let url = window.location.href;
+
+  browser.tabs.query({currentWindow: true, active: true}).then((tabs) => {
+      let tab = tabs[0]; // Safe to assume there will only be one result
+      let qr = "https://api.qrserver.com/v1/create-qr-code/?data="+tab.url;
+
+      var qrImage = document.getElementById("qrImage");
+      qrImage.src = qr;
+  }, console.error)
+}
+
 browser.tabs.executeScript({file: "/content_scripts/generate.js"})
+.then(generateQRCode())
 .catch(reportExecuteScriptError);
-
-let url = window.location.href;
-
-browser.tabs.query({currentWindow: true, active: true}).then((tabs) => {
-    let tab = tabs[0]; // Safe to assume there will only be one result
-    let qr = "https://api.qrserver.com/v1/create-qr-code/?data="+tab.url;
-
-    var qrImage = document.getElementById("qrImage");
-    qrImage.src = qr;
-}, console.error)
